5aeff7df5c586cdbecca0e836a087034ccd18014
ashuOO7 <ashjnavj@gmail.com>
2025-07-03 07:33:24 +0530

    remaining frontend files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Inventory from "./pages/Inventory";
+import NoPageFound from "./pages/NoPageFound";
+import AuthContext from "./AuthContext";
+import ProtectedWrapper from "./ProtectedWrapper";
+import { useEffect, useState } from "react";
+import Store from "./pages/Store";
+import Sales from "./pages/Sales";
+import PurchaseDetails from "./pages/PurchaseDetails";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [user, setUser] = useState("");
+  const [loader, setLoader] = useState(true);
+  let myLoginUser = JSON.parse(localStorage.getItem("user"));
+  // console.log("USER: ",user)
+
+  useEffect(() => {
+    if (myLoginUser) {
+      setUser(myLoginUser._id);
+      setLoader(false);
+      // console.log("here inside effect", myLoginUser fn)
+    } else {
+      setUser("");
+      setLoader(false);
+    }
+  }, [myLoginUser]);
+
+  const signin = (newUser, callback) => {
+    setUser(newUser);
+    callback();
+  };
+
+  const signout = () => {
+    setUser(null);
+    localStorage.removeItem("user");
+  };
+
+  let value = { user, signin, signout };
+
+  if (loader)
+    return (
+      <div
+        style={{
+          flex: 1,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <h1>LOADING...</h1>
+      </div>
+    );
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <AuthContext.Provider value={value}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/"
+            element={
+              <ProtectedWrapper>
+                <Layout />
+              </ProtectedWrapper>
+            }
+          >
+            <Route index element={<Dashboard />} />
+            <Route path="/inventory" element={<Inventory />} />
+            <Route path="/purchase-details" element={<PurchaseDetails />} />
+            <Route path="/sales" element={<Sales />} />
+            <Route path="/manage-store" element={<Store />} />
+          </Route>
+          <Route path="*" element={<NoPageFound />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthContext.Provider>
+  );
+};
 
-export default App
+export default App;
