667d3197e6ca7fb86d3b8bc43c9716aca4b0ce85
ashuOO7 <ashjnavj@gmail.com>
2025-07-03 07:08:22 +0530

    router js files

--- backend/router/purchase.js
+++ backend/router/purchase.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const purchase = require("../controller/purchase");
 
+app.post("/add", purchase.addPurchase);
+
+app.get("/get/:userID", purchase.getPurchaseData);
+app.get("/get/:userID/totalpurchaseamount", purchase.getTotalPurchaseAmount);
 
 module.exports = app;
 
--- backend/router/sales.js
+++ backend/router/sales.js
@@ -2,7 +2,13 @@ const express = require("express");
 const app = express();
 const sales = require("../controller/sales");
 
-// next job is to add sales routes here
+app.post("/add", sales.addSales);
+
+app.get("/get/:userID", sales.getSalesData);
+app.get("/getmonthly", sales.getMonthlySales);
+app.get("/get/:userID/totalsaleamount", sales.getTotalSalesAmount);
+
+module.exports = app;
 
 
 
